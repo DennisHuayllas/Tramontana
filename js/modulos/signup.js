4197d365b6c7bc700fe29976e8a6f410461f93bb
Luigi Martínez <luigi.m.b@hotmail.com>
2021-02-23 17:21:26 +0100

    Version 0.2 complete

--- js/modulos/signup.js
+++ js/modulos/signup.js
@@ -2,10 +2,12 @@
 const esperaPisos = (datos) => {
         M.toast(
             {html: "<span>Usuario dado de alta correctamente.</span>",classes:"green"},2000);
-        setTimeout(() =>{
-            window.location = "pisos.html";
-        }, 2000);
-        console.log(datos);
+
+            setTimeout(()=>{
+
+                window.location = '/pisos'
+            },2000)
+     
 }
 const cargarMenu=()=> {
     fetch("../menu.html")
@@ -23,18 +25,48 @@ const cargarMenu=()=> {
   })
  }
 cargarMenu();
+
+
+
+const validarusuario =async (object,usuario)=>{
+
+    let validar = false 
+    object.forEach(element => {
+    
+       
+        if (element.email == usuario.email ) validar = true
+
+    
+    });
+
+    if(validar == false){
+
+        fetch("http://localhost:3000/usuarios",{
+            method:"POST",
+            body: JSON.stringify(usuario),
+            headers:{
+                "Content-type": "application/json"
+            }
+        })
+        .then(respuesta => respuesta.json())
+        .then(datos => esperaPisos(datos))
+        .catch( error => {
+            M.toast({html: "Problemas con el alta",classes:"red"});
+        });
+
+    } else  M.toast({html: "Este correo ya ha sido utilizado anteriomente",classes:"red"})
+
+
+}
 export const generaAlta = (usuario) => {
     
-    fetch("http://localhost:3000/usuarios",{
-        method:"POST",
-        body: JSON.stringify(usuario),
-        headers:{
-            "Content-type": "application/json"
-        }
-    })
+    let object = null 
+    fetch("http://localhost:3000/usuarios")
     .then(respuesta => respuesta.json())
-    .then(datos => esperaPisos(datos))
-    .catch( error => {
-        M.toast({html: "Problemas con el alta",classes:"red"});
+    .then(datos => validacion = validarusuario(datos,usuario))
+    .catch(error => {
+      
     });
+  
+   
 }
